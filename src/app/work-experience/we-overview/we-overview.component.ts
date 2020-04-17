d128ab2639297c1e35b00ecc923617f6b6d0843d
Ricardo Sanchez <ricardo.sanchez@vhb.com>
2020-04-17 07:44:49 -0500

    add techbridge experience and resume updated

--- src/app/work-experience/we-overview/we-overview.component.ts
+++ src/app/work-experience/we-overview/we-overview.component.ts
@@ -14,7 +14,7 @@ export class WEOverviewComponent implements OnInit {
     neverEnding: 'C# developer of the games, all the logic, and physics of the game. Creating animations and interfaces. Implementing new classes and methods, coroutines, delegates, and events to check changes in the game and trigger something according to that.',
     herbalife: '.NET developer, working on prod support, enhancements and unit testing of the current apps. Creation and consumption of APIs. Creation of new classes and methods for new implementations or features. Help with the SRE certification, developing an editor config to assure coding conventions, making unit testing, and developing a logging system for the application. Creation of interfaces to ensure the implementation of the classes and to facilitate the unit testing.',
     puddingStreet: 'Freelance Project. Development of a web application, with an architecture MVVM using angular, HTML and CSS for the frontend, and for the backend a restful API made with .net core 2.1 in visual studio, and MySQL for the database. I was in charge of the planning, analysis, design, and coding of the app. Deciding which architecture would be better, designing the structure of the app, developing both front and backend, and testing it. Development of the UI.',
-
+    techBridge: 'Creation of a web application, making calls to APIs, receiving and processing the information. Creation of modules, routing modules, components, modals, grids (with Ag-Grid). Managing Communications between components.'
   }
 
   timeline: TimelineElement[] = [];
@@ -27,7 +27,7 @@ export class WEOverviewComponent implements OnInit {
       { caption: 'Jan', date: new Date(2019, 0), endDate: new Date(2019, 5), title: 'NeverEnding Games', position: 'C# Developer', content: this.content.neverEnding },
       { caption: 'May', date: new Date(2019, 4), endDate: new Date(2020, 1), title: 'Herbalife', position: '.NET Developer', content: this.content.herbalife },
       { caption: 'Oct', date: new Date(2019, 9), endDate: new Date(2020, 0), title: 'Pudding Street', position: '.NET Developer FullStack', content: this.content.puddingStreet },
-      { caption: 'May', date: new Date(2020, 1), title: 'TechBridge IT', position: 'Senior Angular Developer', content: this.content.quianty },
+      { caption: 'May', date: new Date(2020, 1), title: 'TechBridge IT', position: 'Senior Angular Developer', content: this.content.techBridge },
     ];
   }
 
